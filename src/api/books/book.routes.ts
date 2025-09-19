4277068d5970708d834feeb16c06bb8308bb148c
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2025-09-19 18:24:44 -0300

    fix(auth): make book search endpoint public
    Removes the authentication middleware from the `/books/search` endpoint,
    transitioning it from a private to a public route.
    
    This change allows unauthenticated users and visitors to search for
    books, improving the discoverability of the platform's content and
    encouraging new user sign-ups.

--- src/api/books/book.routes.ts
+++ src/api/books/book.routes.ts
@@ -2,11 +2,10 @@ import { Router } from "express";
 import { BookController } from "./book.controller.js";
 import { validate } from "#/middlewares/validate.middleware.js";
 import { searchBooksSchema } from "./book.validation.js";
-import { authMiddleware } from "#/middlewares/auth.middleware.js";
 
 const router = Router();
 const bookControler = new BookController();
 
-router.get('/search', authMiddleware, validate(searchBooksSchema), bookControler.searchBooks);
+router.get('/search', validate(searchBooksSchema), bookControler.searchBooks);
 
 export default router;
